1d51dc5a0c7dca89ee704f6186cf79f52ebf45ce
degemarps <degemarpereira@gmail.com>
2021-12-21 20:54:34 -0300

    Typescript usando types e interfaces

--- introducao-typescript/index.ts
+++ introducao-typescript/index.ts
@@ -1,3 +1,38 @@
 function soma(a: number, b: number) {
   return a + b;
 }
+
+console.log(soma(1, 4));
+
+// Types
+// Interfaces
+interface IAnimal {
+  nome: string;
+  tipo: 'terrestre' | 'aquático';
+  domestico: boolean;
+}
+
+interface IFelino extends IAnimal {
+  visaoNoturna: boolean;
+}
+
+interface ICanino extends IAnimal {
+  porte: 'pequeno' | 'medio' | 'grande';
+}
+
+type IDomestico = IFelino | ICanino;
+
+const animal: IDomestico = {
+  domestico: true,
+  nome: 'cachorro',
+  porte: 'medio',
+  tipo: 'terrestre'
+}
+
+// const felino: IFelino = {
+//   nome: 'Leão',
+//   tipo: 'terrestre',
+//   visaoNoturna: true
+// }
+
+console.log(animal);
