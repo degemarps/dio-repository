8b960a1e7b11dc34d788dd59cc6c5012fa577c92
degemarps <degemarpereira@gmail.com>
2021-12-22 15:20:02 -0300

    Criaçao da lista

--- componentes-em-react/react-dio/src/App.js
+++ componentes-em-react/react-dio/src/App.js
@@ -1,6 +1,17 @@
+import Item from './Components/Item';
+import Card from './Components/Card';
+
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <>
+      <h1>Minha primeira aplicação com React</h1>
+      <ul>
+        <Item texto='Item 1' />
+        <Item texto='Item 2' />
+        <Item texto='Item 3'/>
+      </ul>
+      <Card />
+    </>
   );
 }
 
---
+++ componentes-em-react/react-dio/src/Components/Item/index.js
@@ -0,0 +1,9 @@
+const Item = ({texto}) => {
+  return (
+    <a a href="#" className="list-group-item list-group-item-action list-group-item-dark" >
+      {texto}
+    </a>
+  )
+}
+
+export default Item;
