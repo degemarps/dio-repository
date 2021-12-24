1da668841167aec21f50e8903b228157e8a614f4
degemarps <degemarpereira@gmail.com>
2021-12-24 14:48:54 -0300

    Aplicacao para gerar numero randomico

--- aplicacoes-react-native/numeroAleatorio/src/App.js
+++ aplicacoes-react-native/numeroAleatorio/src/App.js
@@ -1,16 +1,47 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, SafeAreaView } from 'react-native';
 
 const App = () => {
+
+  const [numero, setNumero] = useState(0);
+
+  function handleNumero() {
+    const novoNumero = Math.floor(Math.random() * 10);
+
+    setNumero(novoNumero);
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-      }}>
-      <Text>Hello, world!</Text>
-    </View>
+    <SafeAreaView style={style.container}>
+      <Text style={style.numero}>{numero}</Text>
+      <TouchableOpacity onPress={handleNumero} style={style.botao}>
+        <Text>Gerar Número</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   )
 }
+
+// Estilos CSS
+const style = StyleSheet.create({
+  container: {
+    backgroundColor: '#77F',
+    fontSize: 24,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  numero: {
+    fontSize: 32,
+    color: '#FFF',
+  },
+  botao: {
+    backgroundColor: '#FFF',
+    width: '50%',
+    display: 'flex',
+    alignItems: 'center',
+    borderRadius: 5,
+    paddingVertical: 10,
+    marginTop: 20,
+  },
+});
 export default App;
