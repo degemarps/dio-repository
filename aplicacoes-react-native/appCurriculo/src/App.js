aaf089e1233031d26f7a0e3fd6b48bedd4379c4b
degemarps <degemarpereira@gmail.com>
2021-12-25 15:17:16 -0300

    Estilizaçao final do appCurriculo

--- aplicacoes-react-native/appCurriculo/src/App.js
+++ aplicacoes-react-native/appCurriculo/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, Image, SafeAreaView, ScrollView } from 'react-native';
 import photo from './assets/rsz_eu.jpg';
 import Icon from 'react-native-vector-icons/Feather';
 
@@ -8,15 +8,43 @@ const App = () => {
     <>
       <SafeAreaView style={style.page}>
         <View style={style.containerCabecalho}>
-          <Image source={photo} style={style.photo}/>
+          <Image source={photo} style={style.photo} />
           <Text style={style.name}>DEGEMAR PEREIRA DA SILVA</Text>
           <Text style={style.job}>Desenvolvedor Web</Text>
           <View style={style.socials}>
             <Icon name='github' size={30} />
-            <Icon name='facebook' size={30}/>
-            <Icon name='linkedin' size={30}/>
+            <Icon name='facebook' size={30} />
+            <Icon name='linkedin' size={30} />
           </View>
         </View>
+
+        <ScrollView style={style.ScrollView}>
+          <View style={style.cardContainer}>
+            <View style={style.card}>
+              <View style={style.cardHeader}>
+                <Text>Experiência Profissional</Text>
+              </View>
+              <View style={style.cardContent}>
+                <Text style={style.cardContentText}>IFRN</Text>
+                <Text style={style.cardContentText}>SEEC </Text>
+                <Text style={style.cardContentText}>SEMPLA</Text>
+              </View>
+            </View>
+          </View>
+
+          <View style={style.cardContainer}>
+            <View style={style.card}>
+              <View style={style.cardHeader}>
+                <Text>Formação Acadêmica</Text>
+              </View>
+              <View style={style.cardContent}>
+                <Text style={style.cardContentText}>Desenvolvedor Full Stack</Text>
+                <Text style={style.cardContentText}>Superior em Redes</Text>
+                <Text style={style.cardContentText}>Técnico em Eletrônica</Text>
+              </View>
+            </View>
+          </View>
+        </ScrollView>
       </SafeAreaView>
     </>
   );
@@ -26,11 +54,13 @@ const style = StyleSheet.create({
   page: {
     backgroundColor: '#E7E7E7',
     flex: 1,
+    alignItems: 'center',
   },
   containerCabecalho: {
     marginTop: 10,
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
   },
   photo: {
     width: 200,
@@ -51,6 +81,31 @@ const style = StyleSheet.create({
     justifyContent: 'space-between',
     width: '60%',
     marginTop: 20,
+  },
+  ScrollView: {
+    width: '100%',
+    marginBottom: 10,
+  },
+  cardContainer: {
+    width: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  card: {
+    width: '60%',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#939393',
+    backgroundColor: '#FFF',
+    padding: 10,
+  },
+  cardContent: {
+    marginTop: 20,
+  },
+  cardContentText: {
+    color: '#939393',
+    marginBottom: 10,
   },
 });
 
