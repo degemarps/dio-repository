7735f5facb3373647f1b9a525ae6025f31f96719
degemarps <degemarpereira@gmail.com>
2021-12-27 19:18:48 -0300

    Automatizando a forma de desenvolvimento

--- arquitetura-rest-com-nodejs/src/@types/index.ts
+++ arquitetura-rest-com-nodejs/src/@types/index.ts
@@ -1,1 +1,11 @@
-console.log('oi');
+import express, { Request, Response, NextFunction } from 'express';
+
+const app = express();
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: 'Sucesso!' });
+});
+
+app.listen(4000, () => {
+  console.log(`Aplicação executando em http://localhost:4000 !`);
+});
