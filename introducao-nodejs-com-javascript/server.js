6533ee3deeb2206b0674cfe1db82656a239897b6
degemarps <degemarpereira@gmail.com>
2021-12-27 15:55:55 -0300

    Finalizaçao do exercicio

--- introducao-nodejs-com-javascript/server.js
+++ introducao-nodejs-com-javascript/server.js
@@ -1,7 +1,14 @@
 const http = require('http');
 const host = 'http://localhost';
 const port = 3100;
+const stats = require('./pcRamUsage.js');
 
 http.createServer((req, res) => {
-  res.end('<h1>Working!<h1/>');
+  let url = req.url;
+
+    if (url === '/stats') {
+      res.end(JSON.stringify(stats, null, 2));
+    }else {
+      res.end('<h1>Bem vindo(a)!<h1/>')
+    }
 }).listen(port, () => console.log(`Server is running in ${host}:${port}`));
