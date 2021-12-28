2e96ccfa100c0b641ec63bb43c1799d1930676d8
degemarps <degemarpereira@gmail.com>
2021-12-28 16:43:22 -0300

    Refatoraçao do index

--- arquitetura-rest-com-nodejs/src/index.ts
+++ arquitetura-rest-com-nodejs/src/index.ts
@@ -1,18 +1,25 @@
+
 import express from 'express';
-import usersRoute from './routes/users.route';
+import errorHandler from './middlewares/error-handler.middleware';
+import authorizationRoute from './routes/authorization.route';
 import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
 
 const app = express();
 
-// Configuração da aplicação
+// Configurações da aplicação
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Configuração de rotas
-app.use(usersRoute);
+// Configurações de Rotas
 app.use(statusRoute);
+app.use(usersRoute);
+app.use(authorizationRoute);
+
+// Configuração dos Handlers de Erro
+app.use(errorHandler);
 
 // Inicialização do servidor
 app.listen(4000, () => {
-  console.log(`Aplicação executando em http://localhost:4000 !`);
+    console.log('Aplicação executando na porta 3000!');
 });
