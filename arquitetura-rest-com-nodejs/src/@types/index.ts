4f52f37e7aec14c34a68c42762b22480750ee64d
degemarps <degemarpereira@gmail.com>
2021-12-27 21:04:51 -0300

    Finalizando a aplicaçao

--- arquitetura-rest-com-nodejs/src/@types/index.ts
+++ arquitetura-rest-com-nodejs/src/@types/index.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response, NextFunction } from 'express';
+import usersRoute from '../routes/users.route';
 
 const app = express();
+
+// Configuração da aplicação
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configuração de rotas
+app.use(usersRoute);
 
 app.get('/status', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ foo: 'Sucesso!' });
 });
 
+// Inicialização do servidor
 app.listen(4000, () => {
   console.log(`Aplicação executando em http://localhost:4000 !`);
 });
